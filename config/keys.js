1fc58d4f27ccd03c78ca9b3ea80a428f01e09951
unknown <erfanjordison@gmail.com>
2018-06-29 16:35:21 +0600

    mong setup done, connected to app

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://erfan:sXdG1600_6@ds121861.mlab.com:21861/erfan"
+};
--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
 const app = express();
+
+//DB config
+const db = require("./config/keys").mongoURI;
+
+//connect to mogo
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("hello world"));
 
