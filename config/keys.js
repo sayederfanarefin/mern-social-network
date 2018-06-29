790b20cf0bf96a4d95f689c91f31e984885de5eb
unknown <erfanjordison@gmail.com>
2018-06-29 20:06:32 +0600

    generated jwt token

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://erfan:sXdG1600_6@ds121861.mlab.com:21861/erfan"
+  mongoURI: "mongodb://erfan:sXdG1600_6@ds121861.mlab.com:21861/erfan",
+  secretOrKey: "secret"
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
 
 const User = require("../../models/User");
 const router = express.Router();
@@ -64,7 +66,19 @@ router.post("/login", (req, res) => {
     //check password
     bcrypt.compare(password, user.password).then(isMatch => {
       if (isMatch) {
-        res.json({ msg: "success" });
+        //user matched . sign in token
+        const payload = { id: user.id, name: user.name, avatar: user.avatar };
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: "Bearer " + token
+            });
+          }
+        );
       } else {
         return res.status(400).json({ password: "Password incorrect!" });
       }
