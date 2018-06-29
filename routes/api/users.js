edf4ca5285391235936afa771fc92089c4025cdd
unknown <erfanjordison@gmail.com>
2018-06-29 20:35:33 +0600

    added passport and a protected route

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,7 @@ const gravatar = require("gravatar");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
+const passport = require("passport");
 
 const User = require("../../models/User");
 const router = express.Router();
@@ -85,4 +86,17 @@ router.post("/login", (req, res) => {
     });
   });
 });
+
+// @route GET api/users/current
+// @desc Return Current User
+// @access Private
+
+router.get(
+  "/current",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.json(req.user);
+  }
+);
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const passport = require("passport");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
@@ -21,7 +22,10 @@ mongoose
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.log(err));
 
-app.get("/", (req, res) => res.send("hello world"));
+//passport
+app.use(passport.initialize());
+//passport config
+require("./config/passport")(passport);
 
 //user routes
 app.use("/api/users", users);
