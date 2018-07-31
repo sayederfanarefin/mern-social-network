76127a1dae56a261e28444a313ff4175eaa1a865
Sayed Erfan Arefin <erfanjordison@gmail.com>
2018-07-31 09:40:20 +0600

    o

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -3,6 +3,8 @@ const express = require("express");
 const mongoose = require("mongoose");
 const passport = require("passport");
 
+//load validation
+const validateProfileInput = require("../../validation/profile");
 //define router
 const router = express.Router();
 
@@ -48,6 +50,13 @@ router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
+    const { errors, isValid } = validateProfileInput(req.body);
+
+    //check validation
+    if (!isValid) {
+      //return any errors with 400
+      return res.status(400).json(errors);
+    }
     //get fields
     const profileFields = {};
     profileFields.user = req.user.id;
--- validation/profile.js
+++ validation/profile.js
@@ -18,7 +18,41 @@ module.exports = function validateProfileInput(data) {
   if (!Validator.isLength(data.handle, { min: 2, max: 40 })) {
     errors.handle = "Handle must be between 2 and 40 characters";
   }
+  if (Validator.isEmpty(data.skills)) {
+    errors.skills = "Skills is required";
+  }
+  if (Validator.isEmpty(data.website)) {
+    if (Validator.isURL(data.website)) {
+      errors.website = "Not a valid URL";
+    }
+  }
 
+  if (Validator.isEmpty(data.youtube)) {
+    if (Validator.isURL(data.youtube)) {
+      errors.youtube = "Not a valid URL";
+    }
+  }
+
+  if (Validator.isEmpty(data.facebook)) {
+    if (Validator.isURL(data.facebook)) {
+      errors.facebook = "Not a valid URL";
+    }
+  }
+  if (Validator.isEmpty(data.instagram)) {
+    if (Validator.isURL(data.instagram)) {
+      errors.instagram = "Not a valid URL";
+    }
+  }
+  if (Validator.isEmpty(data.linkedin)) {
+    if (Validator.isURL(data.linkedin)) {
+      errors.linkedin = "Not a valid URL";
+    }
+  }
+  if (Validator.isEmpty(data.twitter)) {
+    if (Validator.isURL(data.twitter)) {
+      errors.twitter = "Not a valid URL";
+    }
+  }
   return {
     errors,
     isValid: errors
