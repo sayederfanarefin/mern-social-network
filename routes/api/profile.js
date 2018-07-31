55d4b3a7d33a9fdb82758683f5ccf48c0aa5b2cf
Sayed Erfan Arefin <erfanjordison@gmail.com>
2018-07-31 21:45:37 +0600

    get user by user _id and handle

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -118,4 +118,53 @@ router.post(
   }
 );
 
+// @route GET api/profile/handle/:handle
+// @desc Get profile by handle
+// @access public
+
+router.get(
+  "/handle/:handle",
+
+  (req, res) => {
+    const errors = {};
+
+    profile
+      .findOne({ handle: req.params.handle })
+      .populate("user", ["name", "avatar"])
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = "There is no profile for this user";
+
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
+// @route GET api/profile/user/:user_id
+// @desc Get profile by user_id
+// @access public
+
+router.get(
+  "/user/:user_id",
+
+  (req, res) => {
+    const errors = {};
+
+    profile
+      .findOne({ user: req.params.user_id })
+      .populate("user", ["name", "avatar"])
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = "There is no profile for this user";
+
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
 module.exports = router;
